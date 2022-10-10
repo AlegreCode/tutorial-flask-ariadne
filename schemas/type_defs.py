9c79116865d02bf16bdc7b649cc25848d6346c0a
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-10-10 18:25:34 -0300

    creamos el directorio schemas y definimos el fichero type_defs.py con los tipos de datos.

---
+++ schemas/type_defs.py
@@ -0,0 +1,40 @@
+from ariadne import gql, ScalarType
+from datetime import datetime
+
+datetime_scalar = ScalarType("Datetime")
+@datetime_scalar.serializer
+def datetime_serializer(value):
+    return datetime.strptime(value,"%Y-%m-%dT%H:%M:%S")
+
+type_defs = gql("""
+    scalar Datetime
+
+    type Author {
+        id: ID
+        name: String
+        lastname: String
+        books: [Book]
+        created_at: Datetime
+        updated_at: Datetime
+    }
+
+    type Book {
+        id: ID
+        title: String
+        author: Author!
+        created_at: Datetime
+        updated_at: Datetime
+    }
+
+    type Query {
+        author(id: ID!): Author!
+        authors: [Author]
+        book(id: ID!): Book!
+        books: [Book]
+    }
+
+    type Mutation {
+        author(name: String!, lastname: String!): Author!
+        book(title: String!): Book!
+    }
+""")

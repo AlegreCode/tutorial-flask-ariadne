c6fc501b6cb4609ecf4f92a83020055dffa4ce90
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-10-10 18:56:44 -0300

    quitamos los comentario que vienen por defecto

--- main.py
+++ main.py
@@ -39,20 +39,13 @@ with app.app_context():
 
 @app.route("/graphql", methods=["GET"])
 def graphql_playground():
-    # On GET request serve GraphQL Playground
-    # You don't need to provide Playground if you don't want to
-    # but keep on mind this will not prohibit clients from
-    # exploring your API using desktop GraphQL Playground app.
     return PLAYGROUND_HTML, 200
 
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
-    # GraphQL queries are always sent as POST
     data = request.get_json()
 
-    # Note: Passing the request to the context is optional.
-    # In Flask, the current request is always accessible as flask.request
     success, result = graphql_sync(
         schema,
         data,

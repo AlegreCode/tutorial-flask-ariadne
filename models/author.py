73fbb323e2fb0d1b093e0eecbc7af48bef96d2ba
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-10-12 18:38:24 -0300

    Correción en el modelo Author de server_onupdate por onupdate.

--- models/author.py
+++ models/author.py
@@ -7,4 +7,4 @@ class Author (db.Model):
     lastname= db.Column(db.String(255))
 
     created_at= db.Column(db.DateTime, server_default=db.func.now())
-    updated_at= db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    updated_at= db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())

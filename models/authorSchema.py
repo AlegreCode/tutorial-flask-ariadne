05f023274ed587fc1559cefad4abec3b4b022e70
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-10-12 15:21:31 -0300

    Agregamos el esquema anidado de BookSchema dentro del atributo books del esquema AuthorSchema.
    El atributo books es una array en el que serializamos los libros pertenecientes al author.

--- models/authorSchema.py
+++ models/authorSchema.py
@@ -3,3 +3,5 @@ from models import ma, author
 class AuthorSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = author.Author
+        include_relationships = True
+    books= ma.List(ma.Nested("BookSchema", exclude=("author",)))

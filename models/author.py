7c4089b9fe4a909ecb6719b7e1466c829d87af72
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-10-13 17:43:07 -0300

    Modificamos el modelo Author. Añadimos la relación al modelo Book y la referencia backref author.

--- models/author.py
+++ models/author.py
@@ -5,6 +5,7 @@ class Author (db.Model):
     id= db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String(255))
     lastname= db.Column(db.String(255))
+    books= db.relationship("Book", backref="author", cascade="all, delete-orphan")
 
     created_at= db.Column(db.DateTime, server_default=db.func.now())
     updated_at= db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
